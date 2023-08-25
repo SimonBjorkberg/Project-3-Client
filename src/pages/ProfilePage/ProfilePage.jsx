7f81c2afdd7d5d3f33add8b71c0ef8156648dd20
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-08-25 15:16:54 +0200

    Started on the profile page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,25 @@
 import "./ProfilePage.css";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/auth.context";
+import profileService from "../../services/profile.service";
+import { Link } from "react-router-dom";
 
 function ProfilePage() {
+  const { user } = useContext(AuthContext);
+  const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    profileService.getOne(user._id).then((response) => {
+      setUserData(response.data.user);
+      setLoading(false);
+    });
+  }, [user._id]);
+
+  console.log(userData.products);
   return (
-    <div>
-      <h1>Profile page</h1>
+    <div className="profile page">
+     
     </div>
   );
 }
