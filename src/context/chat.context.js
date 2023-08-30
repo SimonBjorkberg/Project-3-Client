6f6913e787bbed824d20fe3de54bf71e3fbf6ebc
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-08-30 17:26:36 +0200

    deployment

--- src/context/chat.context.js
+++ src/context/chat.context.js
@@ -23,7 +23,7 @@ function ChatProviderWrapper({ children }) {
   const [onlineUsers, setOnlineUsers] = useState([]);
 
   useEffect(() => {
-    const newSocket = io(process.env.REACT_APP_API_URL);
+    const newSocket = io(process.env.REACT_APP_SOCKET_API_URL);
     setSocket(newSocket);
 
     return () => {
