7936644eb55f2681fd005b2973052071d7e8a526
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-08-30 17:32:49 +0200

    testing things

--- src/components/chatComponents/ChatBox.js
+++ src/components/chatComponents/ChatBox.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { AuthContext } from "../../context/auth.context";
 import { ChatContext } from "../../context/chat.context";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
@@ -7,25 +7,39 @@ import UserList from "./UserList";
 
 const ChatBox = () => {
   const { user } = useContext(AuthContext);
-  const { currentChat, messages, userChats, sendTextMessage, isUserChatsLoading, updateCurrentChat } =
-    useContext(ChatContext);
+  const {
+    currentChat,
+    messages,
+    userChats,
+    sendTextMessage,
+    isUserChatsLoading,
+    updateCurrentChat,
+  } = useContext(ChatContext);
   const { recipientUser } = useFetchRecipientUser(currentChat, user);
   const [textMessage, setTextMessage] = useState("");
+
+  const messagesContainerRef = useRef(null)
+
+  useEffect(() => {
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+    }
+  }, [messages])
 
   return (
     <div className="my-10 flex flex-row">
       <div className="w-72 border-t border-b border-l border-neutral">
-      {isUserChatsLoading ? <p>Loading</p> : userChats.map((chat, index) => {
+        {isUserChatsLoading ? (
+          <p>Loading</p>
+        ) : (
+          userChats.map((chat, index) => {
             return (
-              <div
-                key={index}
-                onClick={() => updateCurrentChat(chat)}
-                
-              >
+              <div key={index} onClick={() => updateCurrentChat(chat)}>
                 <UserList user={user} chat={chat} />
               </div>
             );
-          })}
+          })
+        )}
       </div>
       <div className="chat-box border w-96 border-neutral">
         <p className="text-xl border-b pb-2 text-white bg-neutral">
