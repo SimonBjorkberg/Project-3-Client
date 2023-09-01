4ca4f53ff99d037b1764fafd8c0e0aaaa6d99c75
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-09-01 04:48:42 +0200

    merged

--- src/components/chatComponents/ChatBox.js
+++ src/components/chatComponents/ChatBox.js
@@ -4,6 +4,7 @@ import { ChatContext } from "../../context/chat.context";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
 import moment from "moment";
 import ChatDrawerItem from "./ChatDrawerItem";
+import { useNavigate } from "react-router-dom";
 
 const ChatBox = () => {
   const { user } = useContext(AuthContext);
@@ -17,6 +18,11 @@ const ChatBox = () => {
   } = useContext(ChatContext);
   const { recipientUser } = useFetchRecipientUser(currentChat, user);
   const [textMessage, setTextMessage] = useState("");
+  const navigate = useNavigate()
+
+  const handleNavigate = () => {
+    navigate('/chat');
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,38 +39,11 @@ const ChatBox = () => {
 
   return (
     <div className="flex w-full">
-      <div className="w-full">
-        <div className="text-xl p-4 text-white bg-neutral">
-          {recipientUser ? (
-            <div className="flex flex-row items-center justify-between">
-              <label
-                htmlFor="my-drawer-5"
-                className="drawer-button hover:cursor-pointer lg:hidden"
-              >
-                <p className="text-white rounded-md">{"< "}Contacts</p>
-              </label>
-              <p className="flex-grow text-center lg:pr-0 pr-[96px]">
-                {recipientUser.username}
-              </p>
-            </div>
-          ) : (
-            <div className="flex flex-row items-center justify-between">
-              <label
-                htmlFor="my-drawer-5"
-                className="drawer-button hover:cursor-pointer lg:hidden"
-              >
-                <p className="text-white rounded-md">{"< "}Contacts</p>
-              </label>
-              <p className="flex-grow text-center lg:pr-0 pr-[96px]">
-                Select a contact!
-              </p>
-            </div>
-          )}
-        </div>
-        <div className="overflow-y-scroll max-h-screen" id="chatbox">
-          <ul className="list-none p-0">
-            {messages && messages.length === 0 && (
-              <p className="mt-20">
+      <div className="w-full max-h-[80vh]">
+     {recipientUser ? <p className="absolute w-full top-0 z-10 bg-neutral text-white text-xl p-4"><span className="float-left hover:cursor-pointer" onClick={handleNavigate}>{"<"}</span>{recipientUser.username}</p> : <p className="absolute w-full top-0 z-10 bg-neutral text-white text-xl p-4 text-left hover:cursor-pointer" onClick={handleNavigate}>{"< "}Select a contact</p>}
+        <div className="overflow-y-scroll h-[80vh]" id="chatbox">
+        {messages && messages.length === 0 && (
+              <p className="mt-[35vh]">
                 {recipientUser ? "Start the conversation!" : null}
               </p>
             )}
@@ -83,7 +62,6 @@ const ChatBox = () => {
                 </span>
               </div>
             ))}
-          </ul>
         </div>
         <form onSubmit={handleSubmit} className="flex w-full">
           <input
@@ -107,33 +85,6 @@ const ChatBox = () => {
             Send
           </button>
         </form>
-      </div>
-
-      <div className="drawer absolute">
-        <input id="my-drawer-5" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-side z-20">
-          <label htmlFor="my-drawer-5" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-72 min-h-full bg-base-200 text-base-content">
-            <h1 className="text-xl">My Contacts</h1>
-            {isUserChatsLoading ? (
-              <p>Loading</p>
-            ) : (
-              userChats.map((chat, index) => {
-                return (
-                  <label
-                    key={index}
-                    htmlFor="my-drawer-5"
-                    className="drawer-overlay"
-                  >
-                    <div onClick={() => updateCurrentChat(chat)}>
-                      <ChatDrawerItem user={user} chat={chat} />
-                    </div>
-                  </label>
-                );
-              })
-            )}
-          </ul>
-        </div>
       </div>
     </div>
   );
