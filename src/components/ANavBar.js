9c16e67696f24c8ebe76c9ca29dd52c72956b813
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-09-01 10:54:00 +0200

    added top margin to logout button

--- src/components/ANavBar.js
+++ src/components/ANavBar.js
@@ -77,7 +77,7 @@ const ANavBar = (props) => {
                     Chats
                   </p>
                 </Link>
-                <Link onClick={logOutUser}>
+                <Link onClick={logOutUser} className="mt-12">
                   <p className="bg-red-400 text-white py-2 px-4 rounded-md text-xl hover:bg-red-500">
                     Logout
                   </p>
