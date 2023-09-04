76ab3aa130a202df3ef4b91fa5d5e4120f31d0c3
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-09-04 15:52:48 +0100

    Homepage Fixed

--- src/components/LastProducts/LastProducts.jsx
+++ src/components/LastProducts/LastProducts.jsx
@@ -3,7 +3,7 @@ import "./LastProducts.css";
 
 function LastProducts() {
   return (
-    <div className="block vh-50  ">
+    <div className="block">
       <h2 className="mt-6 mb-4 text-center text-grey-800 font-semibold text-lg border-b w-[90%] mx-auto">Recently Added Products</h2>
       <main className="flex row gap-x-2 overflow-x-scroll mb-8">
         <ProductCardHomepage />
--- src/components/MainBanner/MainBanner.jsx
+++ src/components/MainBanner/MainBanner.jsx
@@ -5,9 +5,9 @@ function MainBanner() {
   return (
     <div className="block">
         <div>  
-          <img src="https://static.vinted.com/assets/seller-promotion/gender_test/a/banner-wide-7403f719caac875cfeea61593da7fc7e7320c126193b4ff654e4397f54d430ae.jpg" className=" h-max-[32rem] w-full"></img>
+          <img src="https://static.vinted.com/assets/seller-promotion/gender_test/a/banner-wide-7403f719caac875cfeea61593da7fc7e7320c126193b4ff654e4397f54d430ae.jpg" className="  w-full xl:h-[42rem]"></img>
         </div>
-        <div className="card bg-base-100 shadow-xl  width-tablette align-items-center-tablette md:hidden ">
+        <div className="card bg-base-100 shadow-xl lg:hidden md:hidden ">
           <div className="card-body">
             <h2 className="card-title">Want to make money?</h2>
             <p>
@@ -22,7 +22,37 @@ function MainBanner() {
           </div>
         </div>
 
-        <div className="card bg-base-100 shadow-xl  width-tablette align-items-center-tablette md:inline-flex absolute top-40 left-10 w-[14rem] sm:hidden">
+        <div className="card bg-base-100 shadow-xl  md:inline-flex absolute top-40 left-10 w-[14rem] sm:hidden lg:hidden">
+          <div className="card-body ">
+            <h2 className="card-title">Want to make money?</h2>
+            <p>
+              If you have baby clothes not used anymore discover how to make
+              money
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">
+                <Link to="/products">Our products</Link>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 shadow-xl  lg:inline-flex absolute top-52 left-10 w-[24rem] sm:hidden md:hidden xl:hidden">
+          <div className="card-body ">
+            <h2 className="card-title">Want to make money?</h2>
+            <p>
+              If you have baby clothes not used anymore discover how to make
+              money
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">
+                <Link to="/products">Our products</Link>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 shadow-xl  lg:hidden sm:hidden md:hidden xl:inline-flex absolute top-52 left-10 w-[32rem] ">
           <div className="card-body ">
             <h2 className="card-title">Want to make money?</h2>
             <p>
@@ -42,6 +72,3 @@ function MainBanner() {
 }
 
 export default MainBanner;
-
-
-// https://static.vinted.com/assets/seller-promotion/gender_test/a/banner-wide-7403f719caac875cfeea61593da7fc7e7320c126193b4ff654e4397f54d430ae.jpg
