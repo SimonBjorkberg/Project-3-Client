4d0c7d31ef1103192cee020494c018856d38e76b
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-09-06 16:16:59 +0200

    you can now press the product but no the buttons

--- src/components/ProfilePageComponents/Products.js
+++ src/components/ProfilePageComponents/Products.js
@@ -1,6 +1,5 @@
 import scrollToTop from "../../utils/ScrollToTop";
-import EditProduct from "./EditProduct";
-import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 
 const Products = ({
   foundUser,
@@ -9,22 +8,18 @@ const Products = ({
   showMore,
   setShowMore,
 }) => {
-  const [productHovered, setProductHovered] = useState(null);
+const navigate = useNavigate()
   return (
     <div className="text-left ml-10 pt-10">
       {!foundUser?.products?.length ? (
         <p>This user has no listed products!</p>
       ) : (
         recentProducts?.map((product) => {
-          const modalId = `${product._id}`;
           return (
             <div
-              onMouseEnter={() => {
-                setProductHovered(product._id);
-              }}
-              onMouseLeave={() => setProductHovered(null)}
               key={product._id}
               className="flex py-2 border-b w-[900px] border-neutral hover:bg-neutral-200 hover:cursor-pointer"
+              onClick={() => navigate(`/product/single/${product._id}`)}
             >
               <div className="avatar">
                 <div className="w-16 rounded-xl">
@@ -41,24 +36,11 @@ const Products = ({
               <p className="my-auto ml-5 text-xl w-20">
                 {product.likes.length} Like/s
               </p>
-              {foundUser?._id === loggedInUser?._id &&
-                productHovered === product._id && (
-                  <div className="my-auto ml-auto flex h-full">
-                    <button
-                      className="mr-2 h-full p-2 bg-neutral rounded-sm text-neutral-300 hover:bg-blue-900"
-                      onClick={() => {
-                        const modal = document.getElementById(modalId);
-                        modal.showModal();
-                      }}
-                    >
-                      Edit
-                    </button>
-                    <button className="mr-2 h-full p-2 bg-neutral rounded-sm text-neutral-300 hover:bg-red-600">
-                      delete
-                    </button>
-                  </div>
-                )}
-              <EditProduct modalId={modalId} product={product} />
+              <div className="flex ml-auto"
+              onClick={(e) => e.stopPropagation()}>
+                <button className="mr-2 h-fit my-auto p-2 bg-neutral-400 z-20">Edit</button>
+                <button className="h-fit my-auto p-2 bg-neutral-400 mr-4 z-20">Delete</button>
+              </div>
             </div>
           );
         })
