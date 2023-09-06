b69871b9f358b3bc5731e96d8ebe79f711bf9b3c
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-09-06 04:18:21 +0200

    fixed homepage picture

--- src/components/MainBanner/MainBanner.jsx
+++ src/components/MainBanner/MainBanner.jsx
@@ -5,11 +5,11 @@ function MainBanner() {
   return (
     <div className="block">
       <div className="lg:h-[55vh] md:h-[40vh] overflow-hidden">
-        <img
-          src="https://static.vinted.com/assets/seller-promotion/gender_test/a/banner-wide-7403f719caac875cfeea61593da7fc7e7320c126193b4ff654e4397f54d430ae.jpg"
-          className="w-full"
-          alt=""
-        ></img>
+          <img
+            src="https://static.vinted.com/assets/seller-promotion/gender_test/a/banner-wide-7403f719caac875cfeea61593da7fc7e7320c126193b4ff654e4397f54d430ae.jpg"
+            className="w-full 2xl:relative 2xl:top-[-20%]"
+            alt=""
+          ></img>
       </div>
       <div className="card bg-base-100 shadow-md rounded-none lg:absolute lg:rounded-md lg:top-56 lg:left-40 lg:w-[500px] lg:flex">
         <div className="card-body ">
