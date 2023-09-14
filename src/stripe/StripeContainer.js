dbbe232305449c173c6d60b239d9df379a2cae31
Raphaël Malet <malet.raphael@gmail.com>
2023-09-14 19:37:49 +0200

    css changes

--- src/stripe/StripeContainer.js
+++ src/stripe/StripeContainer.js
@@ -3,15 +3,16 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { CheckoutForm } from "./CheckoutForm";
 
-const PUBLIC_KEY = "pk_test_51NnxmpF7wzBBsskCBp1iRYIWXUZoRNSGXmfmKAmuUIeAUrAu02waNGhoxm8wMhJVRFecNzG5D7s8npsGShVtnhFD00viNYSpZT";
+const PUBLIC_KEY =
+  "pk_test_51NnxmpF7wzBBsskCBp1iRYIWXUZoRNSGXmfmKAmuUIeAUrAu02waNGhoxm8wMhJVRFecNzG5D7s8npsGShVtnhFD00viNYSpZT";
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
 const Stripe = () => {
   return (
     <div className="min-h-[40rem] pt-20">
-    <Elements stripe={stripeTestPromise} >
-      <CheckoutForm />
-    </Elements>
+      <Elements stripe={stripeTestPromise}>
+        <CheckoutForm />
+      </Elements>
     </div>
   );
 };
